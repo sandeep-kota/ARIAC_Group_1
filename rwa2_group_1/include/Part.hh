5d3c14bc9685d6f025a433f320136cd3d5441fb1
Sandeep Kota <sandeep,kota1997@gmail.com>
2021-03-02 00:51:49 -0600

    Added get function for pose

--- rwa2_group_1/include/Part.hh
+++ rwa2_group_1/include/Part.hh
@@ -4,21 +4,24 @@
 
 #include <iostream>
 #include <geometry_msgs/PoseStamped.h>
+// #include <ros/cosole.h>
 
 namespace rwa2 {
 
     class Part {
     public:
-        explicit Part(std::string type,std::string color, int sensor,
-         geometry_msgs::PoseStamped pose_world): type_{type}, color_{color}, sensor_{sensor}, 
+        explicit Part(std::string part_id, std::string type,std::string color, int sensor,
+         geometry_msgs::PoseStamped pose_world): part_id_{part_id}, type_{type}, color_{color}, sensor_{sensor}, 
          pose_world_{pose_world}{}
 
         void Print_Info();
 
         std::string const get_type_() { return type_;}
+        int const get_sensor() {return sensor_;}
+        std::vector<double> get_pose_world();
 
     private:
-
+        std::string part_id_;
         std::string type_;
         std::string color_;
         int sensor_;
--- rwa2_group_1/src/rwa2_node.cpp
+++ rwa2_group_1/src/rwa2_node.cpp
@@ -42,6 +42,12 @@ enum part_code {
     kGasket
 };
 
+enum color_code {
+  kRed,
+  kBlue,
+  kGreen
+};
+
 /**
  * @brief Start the competition
  * Create a service client to /ariac/start_competition
@@ -129,24 +135,7 @@ public:
     received_orders_.push_back(*msg);
   }
 
-  geometry_msgs::PoseStamped frame_to_world(int i, geometry_msgs::Pose original_pos, geometry_msgs::TransformStamped c_w_transform)
-  {   
-    geometry_msgs::PoseStamped pose_target, pose_rel;  
 
-    pose_rel.header.frame_id = c_w_transform.header.frame_id + "_part_" + std::to_string(i);
-    pose_rel.pose = original_pos;
-    tf2::doTransform(pose_rel, pose_target, c_w_transform);
-
-    return pose_target;
-  }
-
-  part_code hashit (std::string const& partString) {
-    if (partString == "disk") return kDisk;
-    if (partString == "pulley") return kPulley;
-    if (partString == "gasket") return kGasket;
-    if (partString == "piston") return kPiston;
-    if (partString == "gear") return kGear;
-  }
   /**
    * @brief Called when a new Message is received on the Topic /ariac/logical_camera_x
    * 
@@ -159,106 +148,39 @@ public:
   {
     
     logic_call_ ++; 
-    geometry_msgs::TransformStamped c_w_transform {};
+    
+    int pos_t {};
+    int pos_c {};
+    std::string type {};
+    std::string color {};
+    geometry_msgs::PoseStamped world_pose {};
 
-    c_w_transform.header.frame_id = "world_logical_camera_" + std::to_string(sensor_n) + "_frame";
-    c_w_transform.transform.translation.x = msg->pose.position.x;
-    c_w_transform.transform.translation.y = msg->pose.position.y;
-    c_w_transform.transform.translation.z = msg->pose.position.z;
-
-    c_w_transform.transform.rotation.x = msg->pose.orientation.x;
-    c_w_transform.transform.rotation.y = msg->pose.orientation.y;
-    c_w_transform.transform.rotation.z = msg->pose.orientation.z;
-    c_w_transform.transform.rotation.w = msg->pose.orientation.w;
 
     if (msg->models.size() > 0) {
       for (int i = 0; i < msg->models.size(); i++)
         {
-          int pos_t = msg->models.at(i).type.find("_");
-          int pos_c = msg->models.at(i).type.rfind("_");
+          pos_t = msg->models.at(i).type.find("_");
+          pos_c = msg->models.at(i).type.rfind("_");
 
-          std::string type = msg->models.at(i).type.substr(0,pos_t);
-          std::string color = msg->models.at(i).type.substr(pos_c+1);
-          geometry_msgs::PoseStamped world_pose = frame_to_world(i, msg->models.at(i).pose, c_w_transform);
+          type = msg->models.at(i).type.substr(0,pos_t);
+          color = msg->models.at(i).type.substr(pos_c+1);
+          world_pose = frame_to_world(i, msg->models.at(i).pose, c_w_transforms_.at(sensor_n));
 
-          rwa2::Part part (type, color, sensor_n, world_pose);
-
-          switch(hashit(type)) {
-            case kPiston:
-              if (color == "red") {
-                parts_.at(0).at(0).push_back(part);
-              } else if (color == "blue") {
-                parts_.at(0).at(1).push_back(part);
-              } else {
-                parts_.at(0).at(2).push_back(part);
-              }       
-              break; 
-            case kDisk:
-            if (color == "red") {
-                parts_.at(1).at(0).push_back(part);
-              } else if (color == "blue") {
-                parts_.at(1).at(1).push_back(part);
-              } else {
-                parts_.at(1).at(2).push_back(part);
-              }       
-              break;
-            case kGasket:
-              if (color == "red") {
-                  parts_.at(2).at(0).push_back(part);
-                } else if (color == "blue") {
-                  parts_.at(2).at(1).push_back(part);
-                } else {
-                  parts_.at(2).at(2).push_back(part);
-                }       
-                break;
-            case kGear:
-              if (color == "red") {
-                  parts_.at(3).at(0).push_back(part);
-                } else if (color == "blue") {
-                  parts_.at(3).at(1).push_back(part);
-                } else {
-                  parts_.at(3).at(2).push_back(part);
-                }       
-                break;
-            default:
-              if (color == "red") {
-                  parts_.at(4).at(0).push_back(part);
-                } else if (color == "blue") {
-                  parts_.at(4).at(1).push_back(part);
-                } else {
-                  parts_.at(4).at(2).push_back(part);
-                }       
-                break;         
-            }
-          
+          save_part_array(type, color, sensor_n, world_pose);                    
         }
     }
-    if (logic_call_ == 14){
-      std::cout << logic_call_ << std::endl;
-      for (int p = 0; p < 5; p++) {
+    if (logic_call_ == 17){
+      remove_duplicate_parts();
+      print_part_poses();
 
-        std::cout << "\n\n*****  " + part_names_.at(p) + " (" + std::to_string(parts_.at(p).at(0).size()+parts_.at(p).at(1).size()
-                  +parts_.at(p).at(2).size()) + " parts)" + "  *****" <<std::endl;
 
-        for (int c = 0; c < 3; c++) {
-
-          std::cout << "================\n" + color_names_.at(c)
-           + " (" + std::to_string(parts_.at(p).at(c).size()) + " parts)\n================" <<std::endl;
-
-            for (int i = 0; i < parts_.at(p).at(c).size(); i++) {
-
-              parts_.at(p).at(c).at(i).Print_Info();
-
-              }
-            }
-          }
       for (int p = 0; p < 5; p++) {
         for (int c = 0; c < 3; c++) {
           parts_.at(p).at(c).clear();
             }
           }
       logic_call_ = 0;
-        }       
+          }       
   }
 
   /**
@@ -292,14 +214,150 @@ public:
     }
   }
 
+  /**
+   * @brief [brief description]
+   * @details [long description]
+   * 
+   * @param i [description]
+   * @param original_pos [description]
+   * @param c_w_transform [description]
+   * @return [description]
+   */
+  geometry_msgs::PoseStamped frame_to_world(int i, geometry_msgs::Pose original_pos, geometry_msgs::TransformStamped c_w_transform)
+  {   
+    geometry_msgs::PoseStamped pose_target, pose_rel;  
+
+    pose_rel.header.frame_id = c_w_transform.header.frame_id + "_part_" + std::to_string(i);
+    pose_rel.pose = original_pos;
+    tf2::doTransform(pose_rel, pose_target, c_w_transform);
+
+    return pose_target;
+  }
+
+  /**
+   * @brief Maps part type to keys. 
+   */
+  part_code hashit_type (std::string const& partString) {
+    if (partString == "disk") return kDisk;
+    if (partString == "pulley") return kPulley;
+    if (partString == "gasket") return kGasket;
+    if (partString == "piston") return kPiston;
+    if (partString == "gear") return kGear;
+  }
+
+  /**
+   * @brief Maps part color to keys. 
+   */
+  color_code hashit_color (std::string const& colorString) {
+    if (colorString == "red") return kRed;
+    if (colorString == "blue") return kBlue;
+    if (colorString == "green") return kGreen;
+  }
+
+
+  /**
+   * @brief Used to save the parts into the array.
+   * @details [long description]
+   * 
+   * @param type [description]
+   * @param color [description]
+   * @param sensor_n [description]
+   * @param world_pose [description]
+   */
+  void save_part_array(std::string type, std::string color, int sensor_n, geometry_msgs::PoseStamped world_pose) {
+    std::string part_id {};
+    int ktype = hashit_type(type);
+    int kcolor =hashit_color(color);
+    // std::cout << kcolor <<std::endl;
+    part_id = type + color + "part" + std::to_string(parts_.at(ktype).at(kcolor).size());
+
+    rwa2::Part part (part_id, type, color, sensor_n, world_pose);
+
+    parts_.at(ktype).at(kcolor).push_back(part); 
+  } 
+
+/**
+ * @brief Rmove duplicate parts added to the list
+ * @details Checks for similar poses occuring in adjacent cameras
+ */
+  void remove_duplicate_parts() {
+    for (int i=0; i<5;i++) {
+      for (int j=0; j<3; j++) {
+      // std::cout<<i<<" "<<j<<" "<<parts_[i][j].size()<<std::endl;
+        for (int k1=0; k1<parts_[i][j].size();k1++) {
+          for (int k2=0; k2<parts_[i][j].size();k2++) {
+            std::vector<double> p1 = parts_[i][j][k1].get_pose_world();
+            std::vector<double> p2 = parts_[i][j][k2].get_pose_world();
+            if (p1[0]==p2[0] && p1[1]==p2[1] && p1[2]==p2[2] 
+                  && std::abs(parts_[i][j][k1].get_sensor() - parts_[i][j][k2].get_sensor())==1) {
+              std::cout<<"Duplicate"<<std::endl;
+              std::cout<<"P1 "<<"x :"<<p1[0]<<"y :"<<p1[1]<<"z :"<<p1[2]<<std::endl;
+              std::cout<<"P2 "<<"x :"<<p2[0]<<"y :"<<p2[1]<<"z :"<<p2[2]<<std::endl;
+              parts_[i][j].erase(parts_[i][j].begin()+k2);
+              std::cout<<"!!!"<<std::endl;
+              break;
+            }
+          }
+        }        
+      }
+    }
+  }
+
+  /**
+   * 
+   */
+  void print_part_poses(std::string p_color = "all",std::string p_type = "all", int verbose=0) {
+    
+      int p_it_min=0;
+      int p_it_max=5;
+      int c_it_min=0;
+      int c_it_max=3;
+
+      
+      if (p_type!="all") {
+        p_it_min = hashit_type(p_type);
+        p_it_max = hashit_type(p_type)+1;
+      } 
+
+      if (p_color!="all") {
+        c_it_min = hashit_color(p_color);
+        c_it_max = hashit_color(p_color)+1;
+      } 
+
+      for (int p = p_it_min; p < p_it_max; p++) {
+        // std::cout << "\n\n*****  " + part_names_.at(p) + " (" + std::to_string(parts_.at(p).at(0).size()+parts_.at(p).at(1).size()
+        //           +parts_.at(p).at(2).size()) + " parts)" + "  *****" <<std::endl;
+        
+        for (int c = c_it_min; c < c_it_max; c++) {
+          std::cout << "================\n" + color_names_.at(c)
+           + " " + part_names_.at(p) + " (" + std::to_string(parts_.at(p).at(c).size()) + " parts)\n================" <<std::endl;
+            
+            if (verbose==0) {
+              for (int i = 0; i < parts_.at(p).at(c).size(); i++) {
+                std::cout<< i <<std::endl;
+                parts_.at(p).at(c).at(i).Print_Info();
+              }
+            }
+        }
+      }
+    
+    std::cout << "------------!!!----------" << std::endl;
+  }
+
+  void set_c_w_transform (int c, geometry_msgs::TransformStamped transform) {
+    c_w_transforms_.at(c) = transform;
+  }
+
 private:
   std::string competition_state_;
   double current_score_;
   std::vector<nist_gear::Order> received_orders_; 
   std::array <std::array <std::vector < rwa2::Part >, 3>, 5> parts_ {}; //Datastructure to store the info from each part detected by the sensors
   int logic_call_ {0};
-  std::array <const std::string, 5> part_names_ {"PISTON", "DISK", "GASKET", "GEAR", "PULLEY"};
+  std::array <const std::string, 5> part_names_ {"DISK", "PULLEY", "PISTON", "GEAR", "GASKET"};
   std::array <const std::string, 3> color_names_ {"Red", "Blue", "Green"};
+  std::array <geometry_msgs::TransformStamped, 17> c_w_transforms_ {}; 
+
 };
 
 int main(int argc, char **argv)
@@ -342,7 +400,7 @@ int main(int argc, char **argv)
 
   // Subscribe to the '/ariac/logical_camera_12' Topic.
 
-  ros::Subscriber logical_camera_0_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
+   ros::Subscriber logical_camera_0_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
       "/ariac/logical_camera_0", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 0));
   
   ros::Subscriber logical_camera_1_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
@@ -354,36 +412,44 @@ int main(int argc, char **argv)
   ros::Subscriber logical_camera_3_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
       "/ariac/logical_camera_3", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 3));
 
+  ros::Subscriber logical_camera_4_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
+      "/ariac/logical_camera_4", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 4));
+
+  ros::Subscriber logical_camera_5_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
+      "/ariac/logical_camera_5", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 5));
+
+  ros::Subscriber logical_camera_6_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
+      "/ariac/logical_camera_6", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 6));
+
+  ros::Subscriber logical_camera_7_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
+      "/ariac/logical_camera_7", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 7));
+
+  ros::Subscriber logical_camera_8_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
+      "/ariac/logical_camera_8", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 8));
+
+ ros::Subscriber logical_camera_9_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
+      "/ariac/logical_camera_9", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 9));
+
   ros::Subscriber logical_camera_10_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
       "/ariac/logical_camera_10", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 10));
 
   ros::Subscriber logical_camera_11_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
       "/ariac/logical_camera_11", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 11));
 
-  ros::Subscriber logical_camera_20_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
-      "/ariac/logical_camera_20", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 20));
+  ros::Subscriber logical_camera_12_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
+      "/ariac/logical_camera_12", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 12));
 
-  ros::Subscriber logical_camera_21_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
-      "/ariac/logical_camera_21", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 21));
+  ros::Subscriber logical_camera_13_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
+      "/ariac/logical_camera_13", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 13));
 
- ros::Subscriber logical_camera_80_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
-      "/ariac/logical_camera_80", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 80));
+  ros::Subscriber logical_camera_14_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
+      "/ariac/logical_camera_14", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 14));
 
-  ros::Subscriber logical_camera_81_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
-      "/ariac/logical_camera_81", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 81));
+  ros::Subscriber logical_camera_15_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
+      "/ariac/logical_camera_15", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 15));
 
-  ros::Subscriber logical_camera_50_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
-      "/ariac/logical_camera_50", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 50));
-
-  ros::Subscriber logical_camera_51_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
-      "/ariac/logical_camera_51", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 51));
-
-  ros::Subscriber logical_camera_110_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
-      "/ariac/logical_camera_110", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 110));
-
-  ros::Subscriber logical_camera_111_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
-      "/ariac/logical_camera_111", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 111));
-
+  ros::Subscriber logical_camera_16_subcriber = node.subscribe<nist_gear::LogicalCameraImage>(
+      "/ariac/logical_camera_16", 1, boost::bind(&MyCompetitionClass::logical_camera_callback, &comp_class, _1, 16));
   // Subscribe to the '/ariac/laser_profiler_0' Topic.
   // ros::Subscriber laser_profiler_subscriber = node.subscribe(
   //     "/ariac/laser_profiler_0", 10, &MyCompetitionClass::laser_profiler_callback, &comp_class);
@@ -391,13 +457,33 @@ int main(int argc, char **argv)
   ROS_INFO("Setup complete.");
   start_competition(node);
 
-  int i {0};
+  //Get transforms world to logical camera sensors
+  tf2_ros::Buffer tfBuffer;
+  tf2_ros::TransformListener tfListener(tfBuffer);
 
-  ros::Rate r(10);
+  ros::Duration timeout(5.0);
+
+  geometry_msgs::TransformStamped transformStamped;    
+
+  for (int i = 0; i < 17; i++) {
+    try{
+      transformStamped = tfBuffer.lookupTransform("world", "logical_camera_" + std::to_string(i) +"_frame",
+                               ros::Time(0), timeout);
+    }
+    catch (tf2::TransformException &ex) {
+      ROS_WARN("%s",ex.what());
+      ros::Duration(1.0).sleep();
+      continue;
+    }
+    //Initialize attribute that stores the frame transforms to world of each camera
+    comp_class.set_c_w_transform(i, transformStamped);
+  }
+
+  ros::Rate rate(10);
   while (ros::ok())
   {
     ros::spinOnce();
-    r.sleep();
+    rate.sleep();
 
   }
   // ros::spin(); // This executes callbacks on new data until ctrl-c.
